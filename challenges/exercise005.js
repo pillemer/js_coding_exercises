d153b0ca012bca191194fd268d1f148a469d1178
Ariel Pillemer <pillemer@gmail.com>
2021-02-20 17:13:30 +0000

    pass tests: findNextNumber, count1sand0s, reverseNumber and sumArrays without arrow functions

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -1,39 +1,42 @@
 const findNextNumber = (nums, n) => {
   if (nums === undefined) throw new Error("nums is required");
   if (n === undefined) throw new Error("n is required");
-
-  const 
-
-
-  // for (let i = 1; 1 < nums.length; i++) {
-  //   if (nums[i] === n) {
-  //     return nums[i+1]
-  //   }
-  // }
-
-  // nums.forEach( (number, index) => { 
-  //   if (number === n) {
-  //     console.log(nums[index+1])
-  //     return nums[index + 1]
-  //   }  
-  // });
-    
-  });
+  const targetIndex = nums.indexOf(n);
+  if (targetIndex > 0 && (nums[targetIndex + 1])) {
+    return nums[targetIndex + 1]
+  }
+  else {
+    return null
+  }
 };
 
 const count1sand0s = str => {
   if (str === undefined) throw new Error("str is required");
-  // Your code here!
+  const ones = [...str].filter(number => number === '1')  
+  return {
+    0: (str.length - ones.length), 
+    1: ones.length}
 };
 
 const reverseNumber = n => {
   if (n === undefined) throw new Error("n is required");
-  // Your code here!
+  const nArray = [...n.toString()]
+  let reversedArray = []
+  for (let i = nArray.length ; i >= 0; i--) {
+    reversedArray.push(nArray[i])
+  }
+  return +reversedArray.join("")
 };
 
 const sumArrays = arrs => {
   if (arrs === undefined) throw new Error("arrs is required");
-  // Your code here!
+  let sum = 0;
+  for (let arr of arrs) {
+    for (let number of arr) {
+      sum += number;
+    }
+  }
+  return sum
 };
 
 const arrShift = arr => {
