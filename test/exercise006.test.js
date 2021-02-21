b0aae3de9848bc8e658cd6d472a92885ba3b8fdd
Ariel Pillemer <pillemer@gmail.com>
2021-02-21 15:55:47 +0000

    pass first test

--- test/exercise006.test.js
+++ test/exercise006.test.js
@@ -11,9 +11,7 @@ const {
 
 
 describe("sumMultiples", () => {
-    test("return the sum of any numbers which are a multiple of 3 or 5 in the array", () => {
-        expect(sumMultiples([5, 3, 7, 8, 1, 10], 7)).toBe(8);
-        expect(sumMultiples([5, 3, 7, 8, 1, 10], 1)).toBe(10);
-        expect(sumMultiples([4, 22, 654, 123, 65, 23, 40, 1], 22)).toBe(654);
+    test("returns the sum of any numbers which are a multiple of 3 or 5", () => {
+        expect(sumMultiples([5, 3, 7, 8, 1, 10])).toBe(18);
     });
 });
