cb5efcd97b3484de998a12a17b206f779cc0465b
Ariel Pillemer <pillemer@gmail.com>
2021-02-21 16:08:20 +0000

    add second test and fail

--- test/exercise006.test.js
+++ test/exercise006.test.js
@@ -12,4 +12,7 @@ describe("sumMultiples", () => {
         expect(sumMultiples([5, 3, 7, 8, 1, 10])).toBe(18);
         expect(sumMultiples([1, 2, 3, 4, 5, 6])).toBe(14);
     });
+    test("returns 0 if no multiples of 3 or 5", () => {
+        expect(sumMultiples([1,2,4,7,21])).toBe(0);
+    });
 });
