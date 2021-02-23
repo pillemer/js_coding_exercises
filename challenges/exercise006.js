ef2506f0f1378e761aed9acd2d6e462695a44d33
Ariel Pillemer <pillemer@gmail.com>
2021-02-23 15:29:34 +0000

    pass all isItPrime tests

--- challenges/exercise006.js
+++ challenges/exercise006.js
@@ -61,7 +61,7 @@ const getComplementaryDNA = str => {
  */
 const isItPrime = n => {
   if (n === undefined) throw new Error("n is required");
-  for (let i = 3; i < n; i++ ) {
+  for (let i = 2; i < n; i++ ) {
     if (n % i === 0 ){
       return false;
     }
