590a3e49e7a68a70d6695017a2feb3275e015341
Ariel Pillemer <pillemer@gmail.com>
2021-03-01 20:20:04 +0000

    add more test for sumDigits, no refactor needed..

--- test/exercise007.test.js
+++ test/exercise007.test.js
@@ -8,6 +8,9 @@ const {
 
 describe("sumDigits", () => {
         test("returns the sum of all digits in a number", () => {
-            expect(sumDigits([123])).toBe(6);
+            expect(sumDigits(0)).toBe(0);
+            expect(sumDigits(123)).toBe(6);
+            expect(sumDigits(1423)).toBe(10);
+            expect(sumDigits(7290)).toBe(18);
         });
     });
