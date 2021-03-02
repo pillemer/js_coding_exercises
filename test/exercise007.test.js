d3e45bef153dec0c7bfb537330319e015872b008
Ariel Pillemer <pillemer@gmail.com>
2021-03-02 23:16:34 +0000

    fail first test for getScreentimeAlertList

--- test/exercise007.test.js
+++ test/exercise007.test.js
@@ -4,7 +4,7 @@ const {
     getScreentimeAlertList,
     hexToRGB,
     findWinner,
-    } = require("../challenges/exercise007");
+} = require("../challenges/exercise007");
 
 describe("sumDigits", () => {
     test("returns the sum of all digits in a number", () => {
@@ -20,7 +20,7 @@ describe("createRange", () => {
         expect(createRange(0, 6, 1)).toStrictEqual([0, 1, 2, 3, 4, 5, 6]);
         expect(createRange(0, 6, 2)).toStrictEqual([0, 2, 4, 6]);
         expect(createRange(10, 60, 10)).toStrictEqual([10, 20, 30, 40, 50, 60]);
-        
+
     });
     test("returns array of numbers with step 1 if no step given", () => {
         expect(createRange(0, 6)).toStrictEqual([0, 1, 2, 3, 4, 5, 6]);
@@ -32,3 +32,30 @@ describe("createRange", () => {
     });
 
 });
+
+describe("getScreentimeAlertList", () => {
+    const ScreenTimeData = [
+        {
+            username: "beth_1234",
+            name: "Beth Smith",
+            screenTime: [
+                { date: "2019-05-01", usage: { twitter: 34, instagram: 22, facebook: 40 } },
+                { date: "2019-05-02", usage: { twitter: 56, instagram: 40, facebook: 31 } },
+                { date: "2019-05-03", usage: { twitter: 12, instagram: 15, facebook: 19 } },
+                { date: "2019-05-04", usage: { twitter: 10, instagram: 56, facebook: 61 } },
+            ]
+        },
+        {
+            username: "sam_j_1989",
+            name: "Sam Jones",
+            screenTime: [
+                { date: "2019-06-11", usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 10} },
+                { date: "2019-06-13", usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 16} },
+                { date: "2019-06-14", usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 31} },
+            ]
+        },
+    ]
+    test("will return empty array if given date is not in the array", () => {
+        expect(getScreentimeAlertList(ScreenTimeData, "2019-05-10")).toBe([]);
+    });
+})
